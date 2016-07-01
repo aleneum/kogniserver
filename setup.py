4f86c5aa2b503a16c2ae09fcc497fb0288cfe336
Alexander Neumann <aleneum@gmail.com>
2016-07-01 17:22:59 +0200

    version push

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import json
 
 setup(
     name='kogniserver',
-    version='0.1.1',
+    version='0.1.2',
     maintainer='Alexander Neumann',
     maintainer_email='aleneum@gmail.com',
     url='http://github.com/aleneum/kogniserver',
