40aae5592603005a84ee2222eaa07a879f99309d
Alexander Neumann <aleneum@gmail.com>
2017-01-20 17:31:09 +0100

    fix test

--- tests/test_bridge.py
+++ tests/test_bridge.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase
-from kogniserver.services import create_rsb_config, Bridge
+from kogniserver.services import create_rsb_config
+from kogniserver.pubsub import PubSubBridge
 from mock import MagicMock
 
 
@@ -16,21 +17,21 @@ class TestKogniServerBridge(TestCase):
     #def __init__(self, rsb_scope, rsb_config, wamp, message_type, mode=BIDIRECTIONAL, wamp_scope=None):
 
     def test_to_wamp(self):
-        self.b = Bridge('/foo/bar', self.config, MagicMock(), 'string', Bridge.RSB_TO_WAMP)
+        self.b = PubSubBridge('/foo/bar', self.config, MagicMock(), 'string', PubSubBridge.RSB_TO_WAMP)
         self.assertIsNone(self.b.rsb_publisher)
         self.assertEqual(self.b.rsb_scope, '/foo/bar')
         self.assertEqual(self.b.wamp_scope, 'foo.bar')
         self.assertIsNotNone(self.b.rsb_listener)
 
     def test_from_wamp(self):
-        self.b = Bridge('/foo/bar', self.config, MagicMock(), 'string', Bridge.WAMP_TO_RSB)
+        self.b = PubSubBridge('/foo/bar', self.config, MagicMock(), 'string', PubSubBridge.WAMP_TO_RSB)
         self.assertIsNone(self.b.rsb_listener)
         self.assertEqual(self.b.rsb_scope, '/foo/bar')
         self.assertEqual(self.b.wamp_scope, 'foo.bar')
         self.assertIsNotNone(self.b.rsb_publisher)
 
     def test_bidirectional(self):
-        self.b = Bridge('/foo/bar', self.config, MagicMock(), 'string', Bridge.BIDIRECTIONAL)
+        self.b = PubSubBridge('/foo/bar', self.config, MagicMock(), 'string', PubSubBridge.BIDIRECTIONAL)
         self.assertIsNotNone(self.b.rsb_listener)
         self.assertEqual(self.b.rsb_scope, '/foo/bar')
         self.assertEqual(self.b.wamp_scope, 'foo.bar')
