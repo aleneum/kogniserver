e57c31aa7c7b855c2692258e7756e01baceb5bc5
Alexander Neumann <aleneum@gmail.com>
2018-01-17 15:00:56 +0100

    release 0.2.3
    improve assistance

--- src/kogniserver/adm.py
+++ src/kogniserver/adm.py
@@ -39,7 +39,7 @@ def main_entry(args=None):
     else:
         elems = elems[:-1]
     prefix = join("/", *elems)
-
+    serve_path = join(prefix, "var/www/kogniserver")
     config_path = join(prefix, 'etc/crossbar/config.json') if not args.config else args.config
 
     choice = 'n'
@@ -52,13 +52,32 @@ def main_entry(args=None):
             else:
                 input_valid = True
 
+    input_valid = False
+    while not input_valid:
+      serve_path = raw_input("Please enter the directory from which files should be served [%s]:" % serve_path) or serve_path
+      if not exists(serve_path):
+        choice = raw_input("%s does not exist. Should it be created? [y]/n: " % serve_path) or 'y'
+        if choice not in ['y', 'n']:
+            print("please enter 'y' or 'n'.")
+        elif choice == 'y':
+            makedirs(serve_path)
+            input_valid = True
+      else:
+        input_valid = True
+
     if choice in 'y' or args.force:
-        protopath = join(prefix, 'share/rst0.12/proto')
+        protopath = join(prefix, 'share/rst0.17/proto')
         input_valid = False if not args.generate else True
         while not input_valid:
             protopath = raw_input("Location of proto-files? [%s]:" % protopath) or protopath
             if not exists(protopath) and not args.generate:
                 print("%s does not exist!" % protopath)
+                choice = raw_input("Do you want to ommit RST in your configuration? y/[n]:") or 'n'
+                if choice not in ['y', 'n']:
+                  print("please enter 'y' or 'n'.")
+                if choice =='y':
+                  input_valid = True
+                  protopath = None
             else:
                 input_valid = True
 
@@ -66,13 +85,13 @@ def main_entry(args=None):
             print "Config file already exists! Use --force to overwrite."
             return
         else:
-            if not exists(dirname(config_path)):
-                makedirs(dirname(config_path))
+            if not exists(dirname(abspath(config_path))):
+                makedirs(dirname(abspath(config_path)))
 
         ssl_cert = False
         input_valid = False if not args.generate else True
         while not input_valid:
-            ssl_cert = raw_input("Location of TLS certificate (without .crt and .key) if needed."
+            ssl_cert = raw_input("Location of TLS certificate (without .crt and .key) if needed. "
                                  "Leave empty if not needed:") or ssl_cert
             input_valid = True
             if ssl_cert:
@@ -84,7 +103,7 @@ def main_entry(args=None):
         with open(config_path, 'w') as target:
             j = json.loads(CONFIG_JSON)
             paths = j['workers'][0]['transports'][0]
-            paths['paths']['/']['directory'] = join(prefix, paths['paths']['/']['directory'])
+            paths['paths']['/']['directory'] = serve_path
             if protopath:
                 paths['paths']['proto']['directory'] = protopath
             else:
@@ -135,11 +154,6 @@ def check_server(address, port):
         #print "Connection to %s on port %s failed: %s" % (address, port, e)
         return False
 
-if __name__ == '__main__':
-    main_entry()
-
-
-
 CONFIG_JSON = """
 {
   "version": 2,
@@ -153,11 +167,11 @@ CONFIG_JSON = """
               "type": "websocket"
             },
             "/": {
-              "directory": "var/www/kogniserver",
+              "directory": ".",
               "type": "static"
             },
             "proto": {
-              "directory": "share/rst0.12/proto",
+              "directory": ".",
               "type": "static"
             }
           },
@@ -207,3 +221,7 @@ CONFIG_JSON = """
   ]
 }
 """
+
+
+if __name__ == '__main__':
+    main_entry()
